c0c63dd0e1600a0102fd34ba06773e6a11ccd467
YannickSchutzbach <yannick.schutzbach@mni.thm.de>
2022-01-22 02:30:28 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { MainComponent } from './components/main/main.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PhotosComponent } from './components/photos/photos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: AppComponent},
+  { path:'albums', 
+    children:[
+      {path:'', component:AlbumsComponent},
+      {path:'photos/:albumId', component:PhotosComponent}
+    ]},
+  { path:'posts', component: PostsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PhotosComponent } from './components/photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AlbumsComponent,
+    PostsComponent,
+    PhotosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
